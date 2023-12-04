8218bc8317b14e3353d76bcfa1c4fa607f0b8069
Lillymuller <eleltaalemu@gmail.com>
2023-12-04 11:30:12 -0800

    100-print_python_list_info

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,5 @@
-#include "python.h"
-
+#include <python.h>
+#define _POSIX_C_SOURCE 199309L
 /**
  * print_python_list_info - function to print basic info about Python lists
  * @p: input pointer to PyObject or the Python list to print info about
