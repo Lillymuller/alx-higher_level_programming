cfa8eb2a77134dd10852a0abee25ae86f5004edf
Lillymuller <eleltaalemu@gmail.com>
2023-12-04 01:06:25 -0800

    13-is_palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -41,3 +41,30 @@ int is_palindrome(listint_t **head)
 	}
 	return (1);
 }
+
+/**
+ * palindrome_check - recursive function to check if listint list is palidrome
+ * @node_head: pointer to the current head of the list
+ * @cnt: integer count of how many nodes from head to the end to check
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int palindrome_check(listint_t *node_head, int cnt)
+{
+	listint_t *node_end = node_head;
+	int cnt2 = 0;
+
+	/* move node_end  checks list fast two times at a time*/
+	for (cnt2 = 0; cnt2 < cnt; cnt2++)
+		node_end = node_end->next;
+	/* count has successfully reached the middle, checks one step at a time */
+	if (cnt <= 0)
+		return (1);
+	/* if mismatch is found, no palindrome */
+	if (node_head->n != node_end->n)
+		return (0);
+	/*else, not at end of list and keep matching,go forward and call again */
+	node_head = node_head->next;
+	cnt -= 2;
+	return (palindrome_check(node_head, cnt));
+}
