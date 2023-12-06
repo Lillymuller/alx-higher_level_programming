71b80c98177faf516ae425d771f3aa1681962ee9
Lillymuller <eleltaalemu@gmail.com>
2023-12-06 02:55:02 -0800

    2-uniq_add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-	return (sum(set(my_list)))
-
-#Sum = 0
-# for i in set(my_list):
-# Sum += i
-# return(Sum)	
+    summation = 0
+    new_list = sorted(my_list)
+    for i in range(len(new_list)):
+        if i == 0 or new_list[i] != new_list[i - 1]:
+            summation += new_list[i]
+    return(summation)
