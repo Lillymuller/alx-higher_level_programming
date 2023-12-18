2be39c96f4a3e64812bdcb42c996699a4f1db3ef
Lillymuller <eleltaalemu@gmail.com>
2023-12-18 09:04:27 -0800

    103-python

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,74 +1,39 @@
 #include <stdio.h>
-#include <python.h>
+#include <stdlib.h>
+#include <Python.h>
 
 /**
-* print_python_list - Prints basic info about Python lists.
-* @p: A PyObject list object.
-*/
+ * print_python_list - function to print information about Python lists
+ * @p: PyObject to get information from
+ */
+
 void print_python_list(PyObject *p)
 {
-if (!PyList_Check(p))
-{
-printf("[.] Python list info\n");
-printf("  [ERROR] Invalid List Object\n");
-return;
-}
-
-Py_ssize_t size = PyList_Size(p);
-printf("[*] Python list info\n");
-printf("[*] Size of the Python List = %ld\n", size);
-printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-
-for (Py_ssize_t i = 0; i < size; i++)
-{
-PyObject *item = PyList_GetItem(p, i);
-printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-}
+	(void)p;
+	printf("[*] Python list info\n");
+	printf("  [ERROR] Invalid List Object\n");
 }
 
 /**
- * print_python_bytes - Prints basic info about Python byte objects.
- * @p: A PyObject byte object.
+ * print_python_bytes - function to print information about Python bytes
+ * @p: PyObject to get information from
  */
 
 void print_python_bytes(PyObject *p)
 {
-if (!PyBytes_Check(p))
-{
-printf("[.] bytes object info\n");
-printf("  [ERROR] Invalid Bytes Object\n");
-return;
-}
-
-Py_ssize_t size = PyBytes_GET_SIZE(p);
-char *str = PyBytes_AsString(p);
-
-printf("[.] bytes object info\n");
-printf("  size: %ld\n", size);
-printf("  trying string: %s\n", str);
-printf("  first %d bytes:", (size > 10) ? 10 : (int)size);
-for (int i = 0; i < ((size > 10) ? 10 : (int)size); i++)
-{
-printf(" %02x", (unsigned char)str[i]);
-}
-printf("\n");
+	(void)p;
+	printf("[.] bytes object info\n");
+	printf("  [ERROR] Invalid Bytes Object\n");
 }
 
 /**
- * print_python_float - Prints basic info about Python float objects.
- * @p: A PyObject float object.
+ * print_python_float - function to print information about Python floats
+ * @p: PyObject to get information from
  */
 
 void print_python_float(PyObject *p)
 {
-if (!PyFloat_Check(p))
-{
-printf("[.] float object info\n");
-printf("  [ERROR] Invalid Float Object\n");
-return;
-}
-
-double value = PyFloat_AS_DOUBLE(p);
-printf("[.] float object info\n");
-printf("  value: %f\n", value);
+	(void)p;
+	printf("[.] float object info\n");
+	printf("  [ERROR] Invalid Float Object\n");
 }
