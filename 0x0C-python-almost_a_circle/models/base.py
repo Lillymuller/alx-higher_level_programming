d5e5e1c4ef2cdf1af773037404643b4f42040f17
Lillymuller <eleltaalemu@gmail.com>
2024-01-15 08:46:40 -0800

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,7 +46,6 @@ class Base():
         """Returns JSON string representation of list dict"""
         if list_dictionaries is None:
             list_dictionaries = []
-	    return "[]"
         return json.dumps(list_dictionaries)
 
     @staticmethod
