d6e999467cf5438cf6cc184601decb8452ef9417
Lillymuller <eleltaalemu@gmail.com>
2024-01-15 01:50:27 -0800

    turtle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ Saves to CSV and loads from CSV file
 
 import json
 import csv
+import turtle
 
 
 class Base():
@@ -121,3 +122,44 @@ class Base():
                 o = cls.create(**dic)
                 objs.append(o)
         return objs
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draw Rectangles and Squares using the turtle module.
+
+        Args:
+            list_rectangles (list): A list of Rectangle to draw.
+            list_squares (list): A list of Square to draw.
+        """
+        turt = turtle.Turtle()
+        turt.screen.bgcolor("#2980B9")
+        turt.pensize(3)
+        turt.shape("turtle")
+
+        turt.color("#FAFAD2")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turt.color("#b5e3d8")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turtle.exitonclick()
