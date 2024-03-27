f905c574b53e6f89005f6e9002af8e0b77630989
Lillymuller <eleltaalemu@gmail.com>
2024-03-27 06:11:54 -0700

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,16 +8,23 @@ def find_peak(list_of_integers):
         return None
 
     size = len(list_of_integers)
+    """Handle lists with one or two elements"""
     if size == 1:
         return list_of_integers[0]
     elif size == 2:
         return max(list_of_integers)
 
+    """ Get the middle element"""
     mid = int(size / 2)
     peak = list_of_integers[mid]
+    """Check if the middle element is the peak"""
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
-    elif peak < list_of_integers[mid - 1]:
+
+    """look for the peak in the left or right middle of the list"""
+    """depending on which is greater than the middle element"""
+
+    if peak < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
     else:
         return find_peak(list_of_integers[mid + 1:])
