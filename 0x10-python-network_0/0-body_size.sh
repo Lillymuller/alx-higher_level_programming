3ecdcca4e316b31f4d1f7bcba95cbc78421e391f
Lillymuller <eleltaalemu@gmail.com>
2024-03-27 05:09:32 -0700

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a request to that URL, and displays the size of the body
-#The size must be displayed in bytes.
+# sends a request to that URL, and displays the size in bytes.
 curl -s "$1" | wc -c
