f757e5711cc1f988cc8b809b4781920f8c851b3a
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 05:07:55 -0700

    8-json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -19,25 +19,20 @@ if __name__ == "__main__":
     else:
         q=""
 
-"""Build the URL with query"""
 url = f"http://0.0.0.0:5000/search_user?=q{q}"
-
-"""Send a Post Request"""
 try:
     response = requests.post(url)
     response.raise_for_status()
 except requests.rxceptions.RequestException as e:
     print(f"Error sending request: {e}")
 
-"""checking for valid Json response"""
-try:
-    data = responce.json()
-except json.JSONDecodeError:
+    try:
+        data = responce.json()
+    except json.JSONDecodeError:
     print("Not a Vaild Json")
 
-"""check for empty responses"""
-if not data:
-    print("No result")
+    if not data:
+        print("No result")
 
-for user in data:
-    print(f"[{user['id']}] {user['name']}")
+        for user in data:
+            print(f"[{user['id']}] {user['name']}")
