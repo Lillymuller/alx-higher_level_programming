66d89786e76a3beefb6897acbabd12231374a523
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 01:37:15 -0700

    post email6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email address
+sends a POST request to the passed URL with the email as a parameter
+And displays content
+"""
+import requests
+from sys imports argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    load = {'email': argv[2]}
+
+    r = requests.post(url, data=load)
+    print(r.text)
