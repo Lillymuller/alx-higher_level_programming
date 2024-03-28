054b55057f3428be99f0bd50937e3196134d554c
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 03:23:47 -0700

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""manage urllib.error.HTTPError exceptions
+"""
+manage urllib.error.HTTPError exceptions
 And prints the error code
 """
 import urllib.request
@@ -7,14 +8,13 @@ import urllib.error
 from sys import argv
 
 
-if __name__ == "__main__"
-url = argv[1]
-req = urllib.request.Request(argv[1])
+if __name__ == "__main__":
+    url = argv[1]
+    req = urllib.request.Request(argv[1])
 
-try:
-    with urllib.request.urlopen(req) as response:
-        res = response.read().decode('utf-8')
-        print(res)
-
+    try:
+        with urllib.request.urlopen(req) as response:
+            res = response.read().decode('utf-8')
+            print(res)
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
