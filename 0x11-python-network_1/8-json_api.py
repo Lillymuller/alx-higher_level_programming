73522f4d83d72520bc5247350df42ef7459a59d7
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 02:30:09 -0700

    8-json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__"
+"""Check for arguments and set the q param"""
+if  len(argv) > 1:
+    q = argv[1]
+else:
+    q=""
+"""Build the URL with query"""
+url = f"http://0.0.0.0:5000/search_user?=q{q}"
+
+"""Snd a Post Request"""
+try:
+    response = requests.post(url)
+    response.raise_for_status()
+except requests.rxceptions.RequestException as e:
+    print(f"Error sending request: {e}")
+
+"""checking for valid Json response"""
+try:
+    data = responce.json()
+except json.JSONDecodeError:
+    print("Not a Vaild Json")
+
+"""check for empty responses"""
+if not data:
+    print("No result")
+for user in data:
+    print(f"[{user['id']}] {user['name']}")
