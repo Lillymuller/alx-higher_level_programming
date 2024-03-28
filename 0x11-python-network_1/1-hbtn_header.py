6e1111bcdc6b1d22860a55b71d147576d31c3757
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 04:51:52 -0700

    1-hbtn_header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """send a request to the URL and displays the value of the X-Request-Id"""
 import urllib.request
-from os import argv
+from sys import argv
 
 
 if __name__ == "__main__":
