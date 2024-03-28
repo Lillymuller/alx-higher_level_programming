6af8286d14a5961976db2fe52dd646db4f174e7f
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 03:30:05 -0700

    hbtn status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+    res = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print(f"\t- type: {type(r.text)}")
-    print(f"\t- content: {r.text}")
+    print(f"\t- type: {type(res.text)}")
+    print(f"\t- content: {res.text}")
