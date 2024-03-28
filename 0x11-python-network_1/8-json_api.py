e48e4b8448cb1c7d46dbadba8e7c6fde4fe29565
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 08:20:56 -0700

    8-json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 """
-Sends a POST request to http://0.0.0.0:5000/search_user with parameter
+Sends a POST request to http://0.0.0.0:5000/search_user with parameter.
 Args:
 argv
 Return:
-Not a valid JSON if the JSON is invalid
-No result if the JSON is empty
+Print 'Not a valid JSON' if the JSON is invalid
+Print 'No result' if the JSON is empty
 """
 import requests
 from sys import argv
+
 
 if __name__ == "__main__":
     if  len(argv) < 2:
         l = argv[1]
     else:
         l=""
-
-send = {"q": l}
-res = requests.post("http://0.0.0.0:5000/search_user", data=send)
-try:
-    data = response.json()
-    if not data:
-        print("No result")
-    else:
-        print(f"[{data.get('id')}] {data.get('name')}")
-except ValueError:
-    print("Not a Vaild Json")
+    send = {"q": l}
+    res = requests.post("http://0.0.0.0:5000/search_user", data=send)
+    try:
+        data = response.json()
+        if not data:
+            print("No result")
+        else:
+            print(f"[{data.get('id')}] {data.get('name')}")
+    except ValueError:
+        print("Not a Vaild Json")
