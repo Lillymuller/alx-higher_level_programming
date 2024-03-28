670e1e46789e656450fbb0cb4dad1ae86128a248
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 03:11:46 -0700

    hbtn header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,5 +7,4 @@ from os import argv
 if __name__ == "__main__":
     request = urllib.request.Request(argv[1])
     with urllib.request.urlopen(request) as response:
-        """check if the header is there and print it out"""
         print(response.getheader['X-Request-Id'], None)
