b2f9ce5c4e8150331bfcf1fa60ca63e47923aee2
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 10:22:40 -0700

    hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,19 +4,19 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 Args:
     url
 Return:
-Print body
-Print body type
-Print unreadable content
-Print decoded readable content
+- Print body
+- Print body type
+- Print unreadable content
+- Print decoded readable content
 """
 import urllib.request
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(req) as response:
+    r = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(r) as response:
         con = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(con)))
         print("\t- content:{}".format(con))
-        print("\t- utf-8 content:{}".format(con.decode('utf-8')))
+        print("\t- utf8 content:{}".format(con.decode('utf-8')))
