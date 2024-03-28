fe4fabbd63f1fb7510f74825e5b2312c7e264803
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 03:33:10 -0700

    5-hbtn header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,7 +6,7 @@ import requests
 from sys import argv
 
 
-if __name__ == "__main__"
-r = requests.get(argv[1])
-res = r.headers.get('X-Request-Id')
-print(res)
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    res = r.headers.get('X-Request-Id')
+    print(res)
