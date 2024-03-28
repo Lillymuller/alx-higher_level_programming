b77e571c66156a8e90be7d0a2fe638d42600b452
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 05:20:52 -0700

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 """Uses GitHub API to show GitHub ID based on  credentials"""
-import 
+import requests
+from sys import argv
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    res = requests.get(url, auth)
+    print(res.json().get("id")
