207760980dab25217acabe346814d209204ddf0c
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 02:44:38 -0700

    hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+Prints the body
+prints the body type
+prints unreadable content
+prints decoded readable content
+"""
 import urllib.request
 
 
 if __name__ == "__main__":
-    """Fetch status form the given URL and display it's content"""
+
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         html = response.read()
-        """printing the body content"""
         print("Body response:")
-        """lets check the body type in bytes"""
         print(f"\t- type: {type(body)}")
-        """the raw content unreadable for text data"""
         print(f"\t- content: {body}")
-        """the readable decoded content using UTF-8 encoding"""
         print(f"\t- utf-8 content: {body.decode('utf-8')}") 
