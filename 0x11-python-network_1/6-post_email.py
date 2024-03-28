7be1fbd54fa451b97fdcaef70e1fa66041a806da
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 14:17:16 -0700

    6-post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,6 +3,12 @@
 takes in a URL and an email address
 sends a POST request to the passed URL with the email parameter
 And displays content
+
+Args:
+- URL
+- Email
+Return:
+- content
 """
 import requests
 from sys import argv
