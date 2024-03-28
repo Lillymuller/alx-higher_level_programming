4686a05db3025b723ffa51d5e98cfef26d1a7c2c
Lillymuller <eleltaalemu@gmail.com>
2024-03-28 11:23:32 -0700

    github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Takes 2 arguments in order to solve this challenge.
+Args:
+- repository name
+- owner name
+"""
+import requests
+from sys import argv
+
+
+base_url = "https://api.github.com/repos/{}/{}/commits/latest"
+
+if __name__ == "__main__":
+    owner = argv[1]
+    repo = argv[2]
+
+    url = base_url.format(owner, repo)
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+
+        data = response.json()
+        commit_message = data["commit"]["message"]
+        print(f"Commit message for {owner}/{repo}: {commit_message}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+    except KeyError:
+        print(f"Error: Invalid data format or missing key in response.")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
