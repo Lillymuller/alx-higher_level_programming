a16f6cd732dc5a574a3ddca388f1a50ed9a47280
Lillymuller <eleltaalemu@gmail.com>
2024-03-29 07:01:28 -0700

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,16 +7,25 @@ Base64 is encoding used in HTTPBasicAuth, it should be used with HTTPS
 Args:
 - url
 - author name
+- token
 Return:
 - ID
 """
 import requests
 from sys import argv
+from sys import exit
 from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
+    """checking argument count"""
+    if len(argv) != 3:
+        exit(f"Usage: script.py <username> <personal_access_token>")
     url = "https://api.github.com/user"
     name = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     res = requests.get(url, auth=name)
-    print(res.json().get("id"))
+
+    if res.status_code == 200:
+        data = res.json()
+    else:
+        print(f"Error: Request failed status code: {res.status_code}")
