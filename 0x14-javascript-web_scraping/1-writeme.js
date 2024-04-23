d0b4459a57e859931c898d4703b2ef30bbffb410
Lillymuller <eleltaalemu@gmail.com>
2024-04-23 02:56:41 -0700

    writing to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (error, body) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log(body);
+  });
