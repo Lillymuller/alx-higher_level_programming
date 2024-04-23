005deed4f915205140841b6fe674c4d63c0fafd2
lillymuller <eleltaalemu@gmail.com>
2024-04-23 03:46:44 -0700

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8',
-	function (error, body) {
-		if (error) {
-			console.log(error)
-			return;
-		} else {
-			console.log(body);
-		}
-	});
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
