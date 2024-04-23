ca20903b8ae53c95ce1db8b625aa2c76f7610073
lillymuller <eleltaalemu@gmail.com>
2024-04-23 03:22:26 -0700

    writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ let fpath = process.argv[2];
 let text = process.argv[3];
 const fs = require('fs');
 fs.writeFile(fpath, text, 'utf8', function (error) {
-  if (error) {
-    console.log(error);
-  }
+	if (error) {
+		console.log(error);
+	}
 });
