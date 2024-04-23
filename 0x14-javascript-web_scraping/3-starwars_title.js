3a937e3157f7d80ceaad6196f23126235648eec4
lillymuller <eleltaalemu@gmail.com>
2024-04-23 03:59:16 -0700

    starwars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 
-let id = process.argv[2];
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request.get(url, (error, response, body) => {
+request.get(url, (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const data = JSON.parse(body);
-    console.log(data.title);
+    const content = JSON.parse(body);
+    console.log(content.title);
   }
 });
