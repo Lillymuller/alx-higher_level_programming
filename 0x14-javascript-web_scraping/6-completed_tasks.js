3a327bb90365e854f1a9370cddc7d3d653fd6325
lillymuller <eleltaalemu@gmail.com>
2024-04-23 04:21:54 -0700

    completed task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,23 @@
 #!/usr/bin/node
+// web scrapper task complited
+
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
+
+request.get(process.argv[2], { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
   }
+
+  const Completed = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!Completed[todo.userId]) {
+        Completed[todo.userId] = 1;
+      } else {
+        Completed[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(Completed);
 });
