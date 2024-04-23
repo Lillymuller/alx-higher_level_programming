da278ecdc6ea8d9f38ded23b13aa0c30e131c77a
lillymuller <eleltaalemu@gmail.com>
2024-04-23 04:35:48 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,37 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.log(error);
+    } else {
+        const content = JSON.parse(body);
+        const characters = content.characters;
+
+        const characterPromises = characters.map(character => {
+            return new Promise((resolve, reject) => {
+                request.get(character, (error, response, body) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        const names = JSON.parse(body);
+                        resolve(names.name);
+                    }
+                });
+            });
+        });
+
+        Promise.all(characterPromises)
+            .then(names => {
+                names.forEach(name => {
+                    console.log(name);
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
-  });
-}
+});
